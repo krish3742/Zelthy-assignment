f036b1e9b0bd771a90e9992dc6f877847b5ecd0a
krish3742 <krish3742@gmail.com>
2025-02-14 20:35:51 +0530

    Set up routing, theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,66 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store";
+import { useSelector } from "react-redux";
+import { ThemeProvider } from "@mui/material/styles";
+import { lightTheme, darkTheme } from "./theme";
+import { SnackbarProvider } from "notistack";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import CssBaseline from "@mui/material/CssBaseline";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import UserProfile from "./components/UserProfile";
+import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+
+function AppContent() {
+  const theme = useSelector((state) => state.user.theme);
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <CssBaseline />
+        <Router>
+          <div>
+            <Navbar />
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/"
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <PrivateRoute>
+                    <UserProfile />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </div>
+        </Router>
+      </LocalizationProvider>
+    </ThemeProvider>
+  );
+}
+
 function App() {
-  return <div>Hey</div>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <SnackbarProvider maxSnack={3}>
+          <AppContent />
+        </SnackbarProvider>
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default App;
